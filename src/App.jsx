457d49f1fc67d021d3067d3f78333324daf589a0
pratik0217 <jadhavpratik0208@gmail.com>
2025-10-06 00:26:51 +0530

    big features section is completed

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,16 @@ import {Resources} from './components/Resources'
 import {Customers} from './components/Customers'
 // import {SignIn} from './components/SignIn'
 // import {SignUp} from './components/SignUp'
+import asset15 from "./homePageAssets/asset\ 15.png"
+import asset16 from "./homePageAssets/asset\ 16.png"
+import asset17 from "./homePageAssets/asset\ 17.png"
+import asset18 from "./homePageAssets/asset\ 18.png"
+import asset19 from "./homePageAssets/asset\ 19.png"
+import asset20 from "./homePageAssets/asset\ 20.png"
+import asset21 from "./homePageAssets/asset\ 21.png"
+import asset22 from "./homePageAssets/asset\ 22.png"
+import asset23 from "./homePageAssets/asset\ 23.png"
+
 
 function App() {
   return (
@@ -67,47 +77,154 @@ function App() {
           </div>
 
           <div className="features-area flex">
-            <div className="features-card">
+            <div className="features-card flex">
               <div className="features-card-img1"></div>
               <h3>Card Sorting</h3>
               <p>Discover how people group and label information.</p>
-              <a href="#"></a>
+              <a className='secondary-btn' href="#">Learn More <i className="fa-solid fa-arrow-right"></i></a>
             </div>
 
-            <div className="features-card">
+            <div className="features-card flex">
               <div className="features-card-img2"></div>
-              <h3>Card Sorting</h3>
+              <h3>Prototype Text</h3>
               <p>Discover how people group and label information.</p>
-              <a href="#"></a>
+              <a className='secondary-btn' href="#">Learn More <i className="fa-solid fa-arrow-right"></i></a>
             </div>
 
-            <div className="features-card">
+            <div className="features-card flex">
               <div className="features-card-img3"></div>
-              <h3>Card Sorting</h3>
+              <h3>First Click Tests</h3>
               <p>Discover how people group and label information.</p>
-              <a href="#"></a>
+              <a className='secondary-btn' href="#">Learn More <i className="fa-solid fa-arrow-right"></i></a>
             </div>
 
-            <div className="features-card">
+            <div className="features-card flex">
               <div className="features-card-img4"></div>
-              <h3>Card Sorting</h3>
+              <h3>Design Surveys</h3>
               <p>Discover how people group and label information.</p>
-              <a href="#"></a>
+              <a className='secondary-btn' href="#">Learn More <i className="fa-solid fa-arrow-right"></i></a>
             </div>
 
-            <div className="features-card">
+            <div className="features-card flex">
               <div className="features-card-img5"></div>
-              <h3>Card Sorting</h3>
+              <h3>Preference Tests</h3>
               <p>Discover how people group and label information.</p>
-              <a href="#"></a>
+              <a className='secondary-btn' href="#">Learn More <i className="fa-solid fa-arrow-right"></i></a>
             </div>
 
-            <div className="features-card">
+            <div className="features-card flex">
               <div className="features-card-img6"></div>
-              <h3>Card Sorting</h3>
+              <h3>Five second tests</h3>
               <p>Discover how people group and label information.</p>
-              <a href="#"></a>
+              <a className='secondary-btn' href="#">Learn More <i className="fa-solid fa-arrow-right"></i></a>
             </div>
+          </div>
+        </div>
+      </section>
+
+      <section id='big-festures'>
+        <div className="container flex big-features-container">
+          <div className='features-img'>
+            <img src={asset15} alt="features-img"/>
+          </div>
+
+          <div className="features-description">
+            <div className="features-descr flex">
+            <h4>Effortless validation for</h4>
+            <h3>Design Professionals</h3>
+            <p>Test interfaces, interaction flows, iconography and more, to help you create intuitive and delightful experiences for your users.</p>
+          </div>
+
+          <div className="features-descr flex">
+            <div className="feature-descr-img">
+              <img src={asset16} alt="" />
+            </div>
+            
+            <div className="guoted-section">
+              <p>The navigation test is god's gift to UI designers, it probably has the best power-to-simplicity ratio of any software, ever.</p>
+            </div>
+            
+            <div className="human-section flex">
+              <div className="human-img">
+                <img src={asset17} alt="human-img" />
+              </div>
+
+              <div className="human-details">
+                <h4>Nick Franklin</h4>
+                <h4>Chief Executive Officer</h4>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+
+        <div className="container flex big-features-container middle-big-features-container">
+          <div className='features-img'>
+            <img src={asset20} alt="features-img"/>
+          </div>
+
+          <div className="features-description">
+            <div className="features-descr flex">
+            <h4>Optimisation for</h4>
+            <h3>Marketers</h3>
+            <p>Fine-tune landing pages, messaging, and creative, helping you optimise conversion rates on marketing campaigns and product launches.</p>
+          </div>
+
+          <div className="features-descr flex">
+            <div className="feature-descr-img">
+              <img src={asset18} alt="" />
+            </div>
+            
+            <div className="guoted-section">
+              <p>We use UsabilityHub tests to help us make decisions for various projects. From web and mobile design to marketing activities.</p>
+            </div>
+            
+            <div className="human-section flex">
+              <div className="human-img">
+                <img src={asset19} alt="human-img" />
+              </div>
+
+              <div className="human-details">
+                <h4>Rick van de Ven</h4>
+                <h4>Digital Marketing Analyst</h4>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+
+        <div className="container flex big-features-container">
+          <div className='features-img'>
+            <img src={asset21} alt="features-img"/>
+          </div>
+
+          <div className="features-description">
+            <div className="features-descr flex">
+            <h4>Easier decision making for</h4>
+            <h3>Product Managers</h3>
+            <p>Ensure you’re delivering the right features to the right customers by validating product concepts with your target audience.</p>
+          </div>
+
+          <div className="features-descr flex">
+            <div className="feature-descr-img">
+              <img src={asset22} alt="" />
+            </div>
+            
+            <div className="guoted-section">
+              <p>Adopting UsabilityHub got us into the habit of asking our users questions before locking in decisions.</p>
+            </div>
+            
+            <div className="human-section flex">
+              <div className="human-img">
+                <img src={asset23} alt="human-img" />
+              </div>
+
+              <div className="human-details">
+                <h4>Ron Diorio</h4>
+                <h4>VP Innovation & New Products</h4>
+              </div>
+            </div>
+          </div>
           </div>
         </div>
       </section>
